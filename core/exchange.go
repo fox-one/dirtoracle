8a0ce3d6daa6fdd4ae78125647009928bbbf2b44
Charlie Wu <wu@fox.one>
2021-04-12 20:40:17 +0800

    DEL: portfolio service;

--- core/exchange.go
+++ core/exchange.go
@@ -11,19 +11,4 @@ type (
 		Name() string
 		GetPrice(ctx context.Context, asset *Asset) (decimal.Decimal, error)
 	}
-
-	PortfolioItem struct {
-		AssetID string          `json:"asset_id"`
-		Amount  decimal.Decimal `json:"amount"`
-	}
-
-	PortfolioToken struct {
-		AssetID string           `json:"asset_id"`
-		Items   []*PortfolioItem `json:"items"`
-	}
-
-	PortfolioService interface {
-		Name() string
-		ListPortfolioTokens(ctx context.Context) ([]*PortfolioToken, error)
-	}
 )
